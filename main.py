c9dfcaf3cf73131761d807147e36fcb19b0249cb
mheann <maryannbernardo212@gmail.com>
2022-06-13 02:07:47 +0800

    create venv for python gui and first program

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+rootWindow = Tk() # create windows widget
+
+messageLabel = Label(rootWindow, text="python desktop GUI tkinter")
+messageLabel.pack()
+
+rootWindow.mainloop()
+
+
+
