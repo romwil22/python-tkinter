951568b1f789d88f5582b7887520e067cd0112db
romwil22 <romwil.pilapil@gmail.com>
2022-06-20 04:25:08 +0800

    done image viewer program and add new code status

--- tkinter_imagesViewer_app.py
+++ tkinter_imagesViewer_app.py
@@ -2,29 +2,62 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 # function
-def next():
-    pass
+def next(imageNumber):
+    global backButton
+    global nextButton
+    global labelImage
+    
+    labelImage.grid_forget()
+    labelImage = Label(image=imageList[imageNumber - 1])
+    
+    backButton = Button(imageViwerWindow, text="<~", command=lambda: back(imageNumber - 1))
+    nextButton = Button(imageViwerWindow, text="~>", command=lambda: next(imageNumber + 1))
+    
+    if (imageNumber == 4):
+        nextButton = Button(imageViwerWindow, text="~>", state=DISABLED)
+    
+    labelImage.grid(row=0, column=0, columnspan=3)
+    backButton.grid(row=1, column=0)
+    nextButton.grid(row=1, column=2)
 
-def back():
-    pass
+def back(imageNumber):
+    global backButton
+    global nextButton
+    global labelImage
+    
+    labelImage.grid_forget()
+    labelImage = Label(image=imageList[imageNumber - 1])
+    
+    backButton = Button(imageViwerWindow, text="<~", command=lambda: back(imageNumber - 1))
+    nextButton = Button(imageViwerWindow, text="~>", command=lambda: next(imageNumber + 1))
+    
+    if (imageNumber == 1):
+        backButton = Button(imageViwerWindow, text="<~", state=DISABLED)
+    
+    labelImage.grid(row=0, column=0, columnspan=3)
+    backButton.grid(row=1, column=0)
+    nextButton.grid(row=1, column=2)
 
 imageViwerWindow = Tk()
 imageViwerWindow.title("Image Viewer")
 imageViwerWindow.iconbitmap("images/favicon.ico")
 
 #image section
-pngImage1 = ImageTk.PhotoImage(Image.open("images/imgsample5.png"))
-pngImage2 = ImageTk.PhotoImage(Image.open("images/imgsample2.png"))
-pngImage3 = ImageTk.PhotoImage(Image.open("images/imgsample3.png"))
-pngImage4 = ImageTk.PhotoImage(Image.open("images/imgsample4.png"))
+pngImage1 = ImageTk.PhotoImage(Image.open("images2/imgsample1.png"))
+pngImage2 = ImageTk.PhotoImage(Image.open("images2/imgsample2.png"))
+pngImage3 = ImageTk.PhotoImage(Image.open("images2/imgsample3.png"))
+pngImage4 = ImageTk.PhotoImage(Image.open("images2/imgsample4.png"))
+
+# list of images
+imageList = [pngImage1, pngImage2, pngImage3, pngImage4]
 
 # label section
 labelImage = Label(image=pngImage1)
 
 # button section
 exitButton = Button(imageViwerWindow, text="Close Program", command=imageViwerWindow.quit)
-backButton = Button(imageViwerWindow, text="<~")
-nextButton = Button(imageViwerWindow, text="~>")
+backButton = Button(imageViwerWindow, text="<~", command=back, state=DISABLED)
+nextButton = Button(imageViwerWindow, text="~>", command=lambda: next(2))
 
 # grid section
 
@@ -35,6 +68,7 @@ labelImage.grid(row=0, column=0, columnspan=3)
 backButton.grid(row=1, column=0)
 exitButton.grid(row=1, column=1)
 nextButton.grid(row=1, column=2)
+
 imageViwerWindow.mainloop()
 
 
---
+++ tkinter_status.py
@@ -0,0 +1,85 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+# function
+def next(imageNumber):
+    global backButton
+    global nextButton
+    global labelImage
+    
+    labelImage.grid_forget()
+    labelImage = Label(image=imageList[imageNumber - 1])
+    
+    backButton = Button(imageViwerWindow, text="<~", command=lambda: back(imageNumber - 1))
+    nextButton = Button(imageViwerWindow, text="~>", command=lambda: next(imageNumber + 1))
+    
+    if (imageNumber == 4):
+        nextButton = Button(imageViwerWindow, text="~>", state=DISABLED)
+    
+    labelImage.grid(row=0, column=0, columnspan=3)
+    backButton.grid(row=1, column=0)
+    nextButton.grid(row=1, column=2)
+    
+    # image number
+    labelStatus = Label(imageViwerWindow, text="images " + str(imageNumber) +"/"+str(len(imageList)), bd=2, relief=SUNKEN, anchor=W)
+    labelStatus.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
+def back(imageNumber):
+    global backButton
+    global nextButton
+    global labelImage
+    
+    
+    labelImage.grid_forget()
+    labelImage = Label(image=imageList[imageNumber - 1])
+    
+    backButton = Button(imageViwerWindow, text="<~", command=lambda: back(imageNumber - 1))
+    nextButton = Button(imageViwerWindow, text="~>", command=lambda: next(imageNumber + 1))
+    
+    if (imageNumber == 1):
+        backButton = Button(imageViwerWindow, text="<~", state=DISABLED)
+    
+    labelImage.grid(row=0, column=0, columnspan=3)
+    backButton.grid(row=1, column=0)
+    nextButton.grid(row=1, column=2)
+    
+    labelStatus = Label(imageViwerWindow, text="images " + str(imageNumber) +"/"+str(len(imageList)), bd=2, relief=SUNKEN, anchor=W)
+    labelStatus.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
+imageViwerWindow = Tk()
+imageViwerWindow.title("Image Viewer")
+imageViwerWindow.iconbitmap("images/favicon.ico")
+
+#image section
+pngImage1 = ImageTk.PhotoImage(Image.open("images2/imgsample1.png"))
+pngImage2 = ImageTk.PhotoImage(Image.open("images2/imgsample2.png"))
+pngImage3 = ImageTk.PhotoImage(Image.open("images2/imgsample3.png"))
+pngImage4 = ImageTk.PhotoImage(Image.open("images2/imgsample4.png"))
+
+# list of images
+imageList = [pngImage1, pngImage2, pngImage3, pngImage4]
+
+# label section
+labelImage = Label(image=pngImage1)
+labelStatus = Label(imageViwerWindow, text="images " + "1" +"/"+str(len(imageList)), bd=2, relief=SUNKEN, anchor=W) 
+
+# button section
+exitButton = Button(imageViwerWindow, text="Close Program", command=imageViwerWindow.quit)
+backButton = Button(imageViwerWindow, text="<~", command=back, state=DISABLED)
+nextButton = Button(imageViwerWindow, text="~>", command=lambda: next(2))
+
+# grid section
+
+# label
+labelImage.grid(row=0, column=0, columnspan=3)
+labelStatus.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
+# buttons
+backButton.grid(row=1, column=0)
+exitButton.grid(row=1, column=1, pady=5)
+nextButton.grid(row=1, column=2)
+
+imageViwerWindow.mainloop()
+
+
+
